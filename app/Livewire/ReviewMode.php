60d433da9c3018c0d894c8944a4b3e7cf53a2ac4
lai <lai@techdrobe.com>
2023-10-09 21:29:54 +0700

    fix (ReviewMode): add check question

--- app/Livewire/ReviewMode.php
+++ app/Livewire/ReviewMode.php
@@ -3,21 +3,27 @@
 namespace App\Livewire;
 
 use App\Models\Exam;
-use App\Models\Question;
 use Livewire\Component;
 
 class ReviewMode extends Component
 {
     public $exam;
     public $questions;
+    public $options;
+
+    public $currentQuestion;
+    public $selectedOptions;
+    public $isCorrectAnswer;
+
+    public $isShowExplaination;
 
     public function mount(
         $exam,
-    ){
+    ) {
         $this->exam = Exam::where('uuid', $exam)->first();
         $this->questions = $this->exam->questions;
-        // dd($this->questions);
-        // $questions = Question::where('exam_id', $)
+        $this->currentQuestion = $this->questions->values()->get(0);
+        $this->options = $this->currentQuestion->options;
     }
 
     public function render()
@@ -25,5 +31,33 @@ class ReviewMode extends Component
         return view('livewire.review-mode')
             ->extends('layouts.master')
             ->section('content');
+    }
+
+    public function checkAnswer()
+    {
+        $this->isShowExplaination = true;
+        $this->isCorrectAnswer = $this->checkCorrectAnswer();
+    }
+
+    private function checkCorrectAnswer()
+    {
+        $this->selectedOptions = is_string($this->selectedOptions) ? [(int)$this->selectedOptions] : $this->selectedOptions;
+
+        $options = $this->options->pluck(null, 'id')->all();
+
+        // if select an item false
+        foreach ($this->selectedOptions as $selectedOption) {
+            if ($options[$selectedOption]->is_correct === 0)
+                return false;
+        }
+
+        // not select true item
+        foreach ($options as $optionId => $option) {
+            if ($option->is_correct === 1 && !in_array($optionId, $this->selectedOptions)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
--- resources/views/livewire/review-mode.blade.php
+++ resources/views/livewire/review-mode.blade.php
@@ -1,3 +1,42 @@
 <div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+    <p>
+        {!! $currentQuestion->text !!}
+    </p>
+
+    <form wire:submit.prevent="checkAnswer">
+        @if ($currentQuestion->is_multichoice)
+            @foreach ($options as $option)
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="{{ $option->id }}" id="flexCheckDefault">
+                    <label class="form-check-label" for="flexCheckDefault">
+                        {!! $option->text !!}
+                    </label>
+                </div>
+            @endforeach
+        @else
+            @foreach ($options as $option)
+                <div class="form-check">
+                    <input wire:model="selectedOptions" class="form-check-input" type="radio"
+                        id="ans-{{ $option->id }}" value="{{ $option->id }}">
+                    <label class="form-check-label" for="ans-{{ $option->id }}">
+                        {!! $option->text !!}
+                    </label>
+                </div>
+            @endforeach
+        @endif
+
+        <button class="btn btn-success" type="submit">Check</button>
+    </form>
+
+    @if ($isShowExplaination)
+        @if ($isCorrectAnswer)
+            <div class="p-3 mt-2 bg-success text-white">Correct</div>
+        @else
+            <div class="p-3 mt-2 bg-danger text-white">Incorrect</div>
+        @endif
+
+        <p>
+            {!! $currentQuestion->explaination !!}
+        </p>
+    @endif
 </div>
