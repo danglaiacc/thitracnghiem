8ddc7ea41d84ac8c72abd2f44c7c0c3bf0419c31
lai <lai@techdrobe.com>
2023-10-10 13:02:12 +0700

    fix (abcd)

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,8 +7,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
     <link href="{{ url('./css/bootstrap.5.3.2.min.css') }}" rel="stylesheet" />
-    <link rel="icon" href="https://getbootstrap.com/docs/5.3/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+    <link rel="icon" href="https://getbootstrap.com/docs/5.3/assets/img/favicons/favicon-32x32.png" sizes="32x32"
+        type="image/png">
     <link rel="stylesheet" href="{{ url('./css/app.css') }}">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+
 
     @stack('css')
 </head>
