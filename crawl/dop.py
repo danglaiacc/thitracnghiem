c8ae59eee64fdb163a7ab847a04526f8fe9c619d
lai <lai@techdrobe.com>
2023-10-20 15:48:43 +0700

    fix (dop): add exam and data

--- crawl/dop.py
+++ crawl/dop.py
@@ -16,3 +16,20 @@ a = ApiFactory(
     raw_data_path=os.path.join(os.getcwd(), 'raw-data', 'dop.data'),
 )
 a.run()
+
+a = ApiFactory(
+    thumbnail='images/aws-dop-2.jpeg',
+    exam_name="DOP Udemy 2",
+    quizz_ids=[
+        5794160,
+        5794162,
+        5794166,
+        5794170,
+        5794174,
+        5794178,
+    ],
+    exam_time=180,
+    subject_id=subject_id,
+    raw_data_path=os.path.join(os.getcwd(), 'raw-data', 'dop.data'),
+)
+a.run()
