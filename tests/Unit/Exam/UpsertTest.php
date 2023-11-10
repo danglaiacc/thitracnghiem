cdd009716da82327ad04b96f61832fea2c538478
lai-2 <lai@techdrobe.com>
2023-11-10 15:34:15 +0700

    chore (UpsertTest): change testcase variable

--- tests/Unit/Exam/UpsertTest.php
+++ tests/Unit/Exam/UpsertTest.php
@@ -12,52 +12,48 @@ use Tests\Unit\BaseTestcase;
 
 class UpsertTest extends BaseTestcase
 {
-    public function test_upsert_exam_create_question()
-    {
-        // create exam and 2 questions
-        $this->createExamAndQuestion();
-
+    private function fakeNewQuestion(string $prefix = "") {
         // create another 2 questions
         $question3 = Question::factory([
-            'text' => 'question 3 text',
-            'explaination' => '333',
+            "text" => "$prefix::question 3 text",
+            "explaination" => "333",
         ])->make();
         $option31 = Option::factory([
-            'text' => 'option31',
-            'is_correct' => 0,
+            "text" => "$prefix::option31",
+            "is_correct" => 0,
         ])->make();
         $option32 = Option::factory([
-            'text' => 'option32',
-            'is_correct' => 0,
+            "text" => "$prefix::option32",
+            "is_correct" => 0,
         ])->make();
         $option33 = Option::factory([
-            'text' => 'option33',
-            'is_correct' => 1,
+            "text" => "$prefix::option33 correct",
+            "is_correct" => 1,
         ])->make();
 
         $question4 = Question::factory([
-            'text' => 'question 4 text',
-            'explaination' => '444',
+            "text" => "$prefix::question 4 text",
+            "explaination" => "444",
         ])->make();
         $option41 = Option::factory([
-            'text' => 'option41',
-            'is_correct' => 0,
+            "text" => "$prefix::option41",
+            "is_correct" => 0,
         ])->make();
         $option42 = Option::factory([
-            'text' => 'option42',
-            'is_correct' => 0,
+            "text" => "$prefix::option42",
+            "is_correct" => 0,
         ])->make();
         $option43 = Option::factory([
-            'text' => 'option43',
-            'is_correct' => 0,
+            "text" => "$prefix::option43",
+            "is_correct" => 0,
         ])->make();
         $option44 = Option::factory([
-            'text' => 'option44',
-            'is_correct' => 1,
+            "text" => "$prefix::option44 correct",
+            "is_correct" => 1,
         ])->make();
         $option45 = Option::factory([
-            'text' => 'option45',
-            'is_correct' => 1,
+            "text" => "$prefix::option45 correct",
+            "is_correct" => 1,
         ])->make();
 
         $fakeQuestion = QuestionHelper::transformQuestions([
@@ -67,41 +63,49 @@ class UpsertTest extends BaseTestcase
                 $question4,
         ]);
 
-        $fakeQuestion[2]['db_status'] = $fakeQuestion[3]['db_status'] = DbStatus::CREATE;
-        $fakeQuestion[2]['options'] = [
+        $fakeQuestion[2]["db_status"] = $fakeQuestion[3]["db_status"] = DbStatus::CREATE;
+        $fakeQuestion[2]["options"] = [
             $option31->toArray(),
             $option32->toArray(),
             $option33->toArray(),
         ];
-        $fakeQuestion[3]['options'] = [
+        $fakeQuestion[3]["options"] = [
             $option41->toArray(),
             $option42->toArray(),
             $option43->toArray(),
             $option44->toArray(),
             $option45->toArray(),
         ];
-        // dd($fakeQuestion);
+
+        return $fakeQuestion;
+    }
+
+    public function test_upsert_exam_create_question()
+    {
+        // create exam and 2 questions
+        $this->createExamAndQuestion();
+        $fakeQuestion = $this->fakeNewQuestion(prefix: 'create');
 
         Livewire::test(Upsert::class, [
-            'exam' => $this->exam
+            "exam" => $this->exam
         ])
-            ->set('questions', $fakeQuestion)
-            ->call('saveExam');
+            ->set("questions", $fakeQuestion)
+            ->call("saveExam");
 
         $this->assertDatabaseHas(
-            'questions',
+            "questions",
             [
-                'text' => $question3->text,
-                'explaination' => $question3->explaination,
-                'is_multichoice' => 0,
+                "text" => "create::question 3 text",
+                "explaination" => '333',
+                "is_multichoice" => 0,
             ],
         );
         $this->assertDatabaseHas(
-            'questions',
+            "questions",
             [
-                'text' => $question4->text,
-                'explaination' => $question4->explaination,
-                'is_multichoice' => 1,
+                "text" => "create::question 4 text",
+                "explaination" => '444',
+                "is_multichoice" => 1,
             ],
         );
     }
